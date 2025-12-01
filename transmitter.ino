76abe4af6cea5935702042849cfa0c3bbfd96f2d
BenjiLimeRidge <benji_salmon@mines.edu>
2025-12-01 16:21:55 -0700

    Refactor OLED display layout and dimensions
    Updated OLED display layout and adjusted dimensions for the dropper box and signal indicator.

--- transmitter.ino
+++ transmitter.ino
@@ -77,49 +77,47 @@ void loop() {
   
   // --- TEMPERATURE BOX ---
   display.drawRect(5, 0, 118, 20, SSD1306_WHITE);
-  display.fillRect(6, 1, 116, 18, SSD1306_BLACK); // clear inside
+  display.fillRect(6, 1, 116, 18, SSD1306_BLACK);
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(8, 4);    
+  display.setCursor(8, 4);
   display.print("Temp: ");
-  if (isnan(temp)) {
-    display.print("Err");
-  } else {
+  if (isnan(temp)) display.print("Err");
+  else {
     display.print(temp, 1);
     display.print("C");
   }
   
   // --- CONDITIONS BOX ---
   display.drawRect(5, 22, 118, 20, SSD1306_WHITE);
-  display.fillRect(6, 23, 116, 18, SSD1306_BLACK); // clear inside
+  display.fillRect(6, 23, 116, 18, SSD1306_BLACK);
   display.setCursor(8, 26);
   display.print("Cond: ");
-  if (isnan(hum)) {
-    display.print("Err");
-  } else {
-    display.print(hum > 60 ? "Wet" : "Dry");
-  }
+  if (isnan(hum)) display.print("Err");
+  else display.print(hum > 60 ? "Wet" : "Dry");
   
   // --- DROPPER BOX ---
-  display.drawRect(5, 44, 118, 20, SSD1306_WHITE);
-  display.fillRect(6, 45, 116, 18, SSD1306_BLACK); // clear inside
+  display.drawRect(5, 44, 118, 18, SSD1306_WHITE);  // make this slightly shorter
+  display.fillRect(6, 45, 116, 16, SSD1306_BLACK);
   display.setCursor(8, 46);
   display.print("Dropper: ");
   display.print(data.ButtonPressed ? "Activated" : "Idle");
   
-  // --- SIGNAL INDICATOR ---
+  // --- SIGNAL INDICATOR BELOW BOXES ---
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(95, 56);
+  display.setCursor(8, 62);  // below the last rectangle
   display.print(signalSent ? "^_^" : "x_x");
   
   display.display();
+
 
   delay(200);
 }
 
 //OLED Display Credit: https://www.instructables.com/Monochrome-096-i2c-OLED-display-with-arduino-SSD13/
 //DHT11 Credit: https://docs.oyoclass.com/unoeditor/Libraries/dht/
+
 
 
 
